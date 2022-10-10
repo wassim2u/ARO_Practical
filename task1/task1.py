ec3a7f1260f2d804945d868610f11e9173de20c9
Wassim Jabrane <wassim.jabrane@gmail.com>
2022-10-10 11:11:36 +0100

    Broken code - but fixed some issues with FK

--- task1/task1.py
+++ task1/task1.py
@@ -64,11 +64,12 @@ sim.getJointLocationAndOrientation("LARM_JOINT5")
 # the base_to_waist translation.
 endEffector = "LARM_JOINT5"
 targetPosition = np.array([0.37, 0.23, 1.06385])  # x,y,z coordinates in world frame
-
 # Example code. Feel free to modify
-# pltTime, pltEFPosition = sim.move_without_PD(endEffector, targetPosition, speed=0.01, orientation=None, threshold=1e-3, maxIter=3000, debug=False, verbose=False)
 
+pltTime, pltEFPosition = sim.move_without_PD(endEffector, targetPosition, speed=0.01, orientation=None, threshold=1e-3, maxIter=3000, debug=True, verbose=False)
 
+jac = sim.jacobianMatrix(endEffector)
+print(jac)
 # # Now plot some graphs
 # task1_figure_name = "task1_kinematics.png"
 # task1_savefig = True
